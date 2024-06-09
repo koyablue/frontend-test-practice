5c0b6ca7a81dfe7312db2ce82ac79ceb9c3b802a
koyaaoyama <aoyama.ko0920@gmail.com>
2024-06-08 22:33:13 -0700

    Implement API success test

--- src/test/PokemonFinder.test.tsx
+++ src/test/PokemonFinder.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "vitest";
 import PokemonFinder from "../components/PokemonFinder";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 
 describe(PokemonFinder, () => {
   test("Initial rendering occurs correctly", () => {
@@ -13,5 +14,25 @@ describe(PokemonFinder, () => {
     expect(
       screen.getByPlaceholderText("ポケモンのIDを入力")
     ).toBeInTheDocument();
+  });
+
+  test("Fetched pokemon data will be displayed", async () => {
+    render(<PokemonFinder />);
+
+    const user = userEvent.setup();
+    const inputElement = screen.getByPlaceholderText("ポケモンのIDを入力");
+    await user.type(inputElement, "25");
+
+    const buttonElement = screen.getByRole("button", {
+      name: "ポケモンを見つける",
+    });
+    await user.click(buttonElement);
+
+    const pokemonName = screen.getByText("pikachu");
+    expect(pokemonName).toBeInTheDocument();
+
+    const image = screen.getByRole("img");
+    expect(image).toHaveAttribute("src", "http://example.com/pikachu.png");
+    expect(image).toHaveAttribute("alt", "pikachu");
   });
 });
--- src/test/mocks/handlers.ts
+++ src/test/mocks/handlers.ts
@@ -1,14 +1,12 @@
 import { http, HttpResponse } from "msw";
 
 const pokemon = {
-  "25": {
-    name: "pikachu",
-    sprites: { front_default: "http://example.com/pikachu.png" },
-  },
+  name: "pikachu",
+  sprites: { front_default: "http://example.com/pikachu.png" },
 };
 
 export const handlers = [
-  http.get("https://pokeapi.co/api/v2/pokemon/${pokemonId}", () => {
+  http.get("https://pokeapi.co/api/v2/pokemon/25", () => {
     if (pokemon) {
       return HttpResponse.json(pokemon, { status: 200 });
     } else {
