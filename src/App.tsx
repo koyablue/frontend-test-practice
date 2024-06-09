e5a1c38da76909794ae4ef861ca9d4456842e5c1
koyaaoyama <aoyama.ko0920@gmail.com>
2024-06-08 19:02:14 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PokemonFinder from "./components/PokemonFinder";
+
+function App() {
+  return (
+    <>
+      <PokemonFinder />
+    </>
+  );
+}
+
+export default App;
